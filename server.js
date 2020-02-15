d611e979b213eedd9b02c85e984e05f71e7b85cb
Joaquim Medeiros <joaomedeiros@brq.com>
2020-02-15 17:45:47 -0300

    update

--- server.js
+++ server.js
@@ -5,14 +5,24 @@ const app = express();
 app.use(compression());
 const fs = require('fs');
 var cors = require('cors');
-app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+
+app.all('*', function(req, res, next) {
+  var origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
+  app.configure(function () {
+    app.use(cors());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(allowCrossDomain);
+    app.use(express.static(path.join(application_root, "public")));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
 app.get('/hotels', function(req, res) {
   if (req.query.q) {
     fs.readFile('./db.json', (err, json) => {
