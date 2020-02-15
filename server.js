b79b3c5ac678f10302de89a0f91bc342915059d1
Joaquim Medeiros <joaomedeiros@brq.com>
2020-02-15 17:08:06 -0300

    create string filter

--- server.js
+++ server.js
@@ -3,39 +3,31 @@ const compression = require("compression");
 const path = require("path");
 const app = express();
 app.use(compression());
-const fs = require('fs');
+const fs = require("fs");
 
-app.get('/hotels', function(req, res) {
-fs.readFile('./db.json', 
-  (err, json) =>  res.json(JSON.parse(json)));
+app.get("/hotels", function(req, res) {
+    if (req.query.q) {
+        fs.readFile("./db.json", (err, json) => {
+            let response = JSON.parse(json);
+            response = response
+            .filter(item => item.name.toLowerCase().indexOf(req.query.q.toLowerCase()) > -1);
+            res.json(response);
+        });
+    } else {
+      fs.readFile("./db.json", (err, json) => res.json(JSON.parse(json)));
+    }
 });
 
-app.get('/hotel/:id', function(req, res) {
-  let response;
-  fs.readFile('./db.json', 
-    (err, json) => {
-      response = JSON.parse(json)
-      //.filter( item => item.id === req.params.id);
-      console.log(req.params.id);
-      response = response.filter (item => item.id  == req.params.id)
-      res.json(response);
+app.get("/hotel/:id", function(req, res) {
+    let response;
+    fs.readFile("./db.json", (err, json) => {
+        response = JSON.parse(json);
+        response = response.filter(item => item.id == req.params.id);
+        res.json(response);
     });
-    
-  });
-  
-
-
-// app.get('/hotel/:id', function (req, res, next) {
-//   let response;
-//   fs.readFile('./db.json', 
-//   (err, json) => response = (JSON.parse(json)));
-//   console.log(response);
-//   res.json(response);
-// })
-  
-
+});
 app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(404).send('Something broke!');
+    console.error(err.stack);
+    res.status(404).send("Something broke!");
 });
 app.listen(process.env.PORT || 3000);
