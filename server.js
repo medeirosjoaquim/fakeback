c4ef31c8e7654a5cd20f60f439137136c7203db3
Joaquim Medeiros <joaomedeiros@brq.com>
2020-02-15 16:19:54 -0300

    Update endpoints

--- server.js
+++ server.js
@@ -6,11 +6,33 @@ app.use(compression());
 const fs = require('fs');
 
 app.get('/hotels', function(req, res) {
-fs.readFile('./db.json', (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
-    });
+fs.readFile('./db.json', 
+  (err, json) =>  res.json(JSON.parse(json)));
 });
+
+app.get('/hotel/:id', function(req, res) {
+  let response;
+  fs.readFile('./db.json', 
+    (err, json) => {
+      response = JSON.parse(json)
+      //.filter( item => item.id === req.params.id);
+      console.log(req.params.id);
+      response = response.filter (item => item.id  == req.params.id)
+      res.json(response);
+    });
+    
+  });
+  
+
+
+// app.get('/hotel/:id', function (req, res, next) {
+//   let response;
+//   fs.readFile('./db.json', 
+//   (err, json) => response = (JSON.parse(json)));
+//   console.log(response);
+//   res.json(response);
+// })
+  
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
