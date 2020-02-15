7f02e524fd9f8ebe9a54da3d05b1f1b97ddfc798
Joaquim Medeiros <joaomedeiros@brq.com>
2020-02-15 17:40:02 -0300

    update

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.use(compression());
 const fs = require('fs');
 var cors = require('cors');
 app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.get('/hotels', function(req, res) {
   if (req.query.q) {
     fs.readFile('./db.json', (err, json) => {
